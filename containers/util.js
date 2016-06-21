2fc11e571873d08e9411328dcfb82409150b4f25
seokgyo <seokgyo@gmail.com>
2016-06-21 17:34:22 +0900

    remove out of stock status.

--- containers/util.js
+++ containers/util.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 
 export function isPickedUp(order) {
-  // 104: OUT_OF_STOCK
   // 200: PICKED_UP
-  return _.every(order.orderProducts, (o) => o.status === 200 || o.status === 104);
+  return _.every(order.orderProducts, (o) => o.status === 200);
 }
